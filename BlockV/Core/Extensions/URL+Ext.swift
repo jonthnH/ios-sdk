e394522dbfbdeb5fdbe69c79b3b55598db98b09f
Cameron McOnie <cjmconie@gmail.com>
2018-07-31 13:06:58 +0200

    add URL extensions to assist with param appending and deleting

--- BlockV/Core/Extensions/URL+Ext.swift
+++ BlockV/Core/Extensions/URL+Ext.swift
@@ -1,21 +1,95 @@
+// MIT License
 //
-//  BlockV AG. Copyright (c) 2018, all rights reserved.
+// Copyright (c) 2015-2018 SwifterSwift (https://github.com/swifterswift)
 //
-//  Licensed under the BlockV SDK License (the "License"); you may not use this file or
-//  the BlockV SDK except in compliance with the License accompanying it. Unless
-//  required by applicable law or agreed to in writing, the BlockV SDK distributed under
-//  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-//  ANY KIND, either express or implied. See the License for the specific language
-//  governing permissions and limitations under the License.
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
 //
+//    The above copyright notice and this permission notice shall be included in
+//    all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//    THE SOFTWARE.
 
 import Foundation
 
-extension URL {
-    func encodeWithParams() -> URL {
-
-        // Encode with blockv url params.
-
-        return self
+// MARK: - Methods
+public extension URL {
+    
+    /// SwifterSwift: URL with appending query parameters.
+    ///
+    ///        let url = URL(string: "https://google.com")!
+    ///        let param = ["q": "Swifter Swift"]
+    ///        url.appendingQueryParameters(params) -> "https://google.com?q=Swifter%20Swift"
+    ///
+    /// - Parameter parameters: parameters dictionary.
+    /// - Returns: URL with appending given query parameters.
+    public func appendingQueryParameters(_ parameters: [String: String]) -> URL {
+        var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: true)!
+        var items = urlComponents.queryItems ?? []
+        items += parameters.map({ URLQueryItem(name: $0, value: $1) })
+        urlComponents.queryItems = items
+        return urlComponents.url!
     }
+    
+    /// SwifterSwift: Append query parameters to URL.
+    ///
+    ///        var url = URL(string: "https://google.com")!
+    ///        let param = ["q": "Swifter Swift"]
+    ///        url.appendQueryParameters(params)
+    ///        print(url) // prints "https://google.com?q=Swifter%20Swift"
+    ///
+    /// - Parameter parameters: parameters dictionary.
+    public mutating func appendQueryParameters(_ parameters: [String: String]) {
+        self = appendingQueryParameters(parameters)
+    }
+    
+    /// SwifterSwift: Get value of a query key.
+    ///
+    ///    var url = URL(string: "https://google.com?code=12345")!
+    ///    queryValue(for: "code") -> "12345"
+    ///
+    /// - Parameter key: The key of a query value.
+    public func queryValue(for key: String) -> String? {
+        let stringURL = self.absoluteString
+        guard let items = URLComponents(string: stringURL)?.queryItems else { return nil }
+        for item in items where item.name == key {
+            return item.value
+        }
+        return nil
+    }
+    /// SwifterSwift: Returns a new URL by removing all the path components.
+    ///
+    ///     let url = URL(string: "https://domain.com/path/other")!
+    ///     print(url.deletingAllPathComponents()) // prints "https://domain.com/"
+    ///
+    /// - Returns: URL with all path components removed.
+    public func deletingAllPathComponents() -> URL {
+        var url: URL = self
+        for _ in 0..<pathComponents.count - 1 {
+            url.deleteLastPathComponent()
+        }
+        return url
+    }
+    
+    /// SwifterSwift: Remove all the path components from the URL.
+    ///
+    ///        var url = URL(string: "https://domain.com/path/other")!
+    ///        url.deleteAllPathComponents()
+    ///        print(url) // prints "https://domain.com/"
+    public mutating func deleteAllPathComponents() {
+        for _ in 0..<pathComponents.count - 1 {
+            deleteLastPathComponent()
+        }
+    }
+
 }
